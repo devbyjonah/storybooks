ef36f7c857929531307b0a58981afd57f8883843
devbyjonah <jonahwagner231@gmail.com>
2022-12-22 01:13:07 -0800

    finished initial setup of google Oauth

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const ejsLayouts = require('express-ejs-layouts')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // intialize environment / DB Connection / App
 dotenv.config({ path: './config/config.env' })
 connectDB()
 const app = express()
+
+// Passport config
+require('./config/passport')(passport)
 
 // logging with morgan module
 if (process.env.NODE_ENV === 'development'){
@@ -21,11 +26,25 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.set('layout', './layouts/main')
 
+// Session middleware
+app.use(
+	session({
+		secret:'el gato',
+		resave: false,
+		saveUninitialized: false
+	})
+)
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 8000
 
