71123f97afd5803a51bf6d0a3c5c59840535b0bf
devbyjonah <jonahwagner231@gmail.com>
2022-12-26 16:24:21 -0800

    setup form for new stories

---
+++ routes/stories.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+router.get('/add', ensureAuth, (req, res) => {
+	res.render('stories/add')
+})
+
+module.exports = router
