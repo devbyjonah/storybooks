a2dd34864858c1ac6d63901d23d2d7672c2be607
devbyjonah <jonahwagner231@gmail.com>
2022-12-28 16:59:08 -0800

    finished edit story route

--- routes/stories.js
+++ routes/stories.js
@@ -48,4 +48,18 @@ router.post('/', ensureAuth, async (req, res) => {
 	}
 })
 
+router.put('/:id', ensureAuth, async (req, res) => {
+	let story = await Story.findById(req.params.id).lean()
+
+	if (!story){
+		return res.render('error/404')
+	}
+	if (story.user != req.user.id){
+		res.redirect('/stories')
+	}else {
+		story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, { new:true, runValidators: true })
+		res.redirect('/dashboard')
+	}
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -20,6 +20,16 @@ const app = express()
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 // Passport config
 require('./config/passport')(passport)
 
